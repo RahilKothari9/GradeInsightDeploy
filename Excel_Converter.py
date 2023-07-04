7971111670dcb6d1c3d7a43fa50eb4e42415a5ae
Rahil Kothari <kotharirahil9@gmail.com>
2023-07-04 16:31:12 +0530

    Add File Input and Display

--- Excel_Converter.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_excel("students.xlsx")
-dict = df.to_dict()
-print(dict)
--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+import os
+from werkzeug.utils import secure_filename
+import pandas as pd
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'xlsx'}
+app.config['UPLOAD_PATH'] = UPLOAD_FOLDER
+
 @app.route("/")
 def hello_world():
+    return redirect("/fileupload")
     return "<p>Hello, World!</p>"
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/fileupload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            # Make a db entry for the file for particular user and subject
+            return redirect(url_for('upload', filename=filename))
+    return render_template('fileupload.html')
+
+@app.route('/viewmarks/<filename>')
+def upload(filename):
+    #get filename from db;
+    filename = 'template.xlsx'
+    xl = 'uploads/'+filename
+    df = pd.read_excel(io = xl)
+    return render_template('viewmarks.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == "__main__":
     app.run(debug="True")
