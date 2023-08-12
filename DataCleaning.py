cbf5108537546c0797c5f921dca0f6fda4ca9688
Rahil Kothari <kotharirahil9@gmail.com>
2023-08-12 14:57:40 +0530

    Fix datacleaning

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_excel('EntryTemplate.xlsx', sheetname='0') # can also index sheet by name or fetch all sheets
-mylist = df['column name'].tolist()
+df = pd.read_excel('EntryTemplate.xlsx') # can also index sheet by name or fetch all sheets
+mylist = df['ROLLNO'].tolist()
 print(mylist)
