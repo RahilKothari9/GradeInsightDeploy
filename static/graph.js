8a826dc1e093c75bc19b9e17531f64d9979390ef
Rahil Kothari <kotharirahil9@gmail.com>
2023-08-16 23:35:14 +0530

    Fix radio + Hide graphs(incomplete)

--- static/graph.js
+++ static/graph.js
@@ -3,4 +3,20 @@ const ese = document.querySelector(".ese")
 const total = document.querySelector(".tot")
 
 
-ise.addEventListener("click",())
+ise.addEventListener("click",()=>{
+    ise.classList.add("show")
+    ese.classList.add("hidden")
+    total.classList.add("hidden")
+})
+ese.addEventListener("click",()=>{
+    ese.classList.add("show")
+    ise.classList.add("hidden")
+    total.classList.add("hidden")
+})
+tot.addEventListener("click",()=>{
+    tot.classList.add("show")
+    ese.classList.add("hidden")
+    ise.classList.add("hidden")
+})
+
+
